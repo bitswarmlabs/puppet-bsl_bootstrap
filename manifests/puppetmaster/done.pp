7cde901376fe8675faa8c748190e225a70db3eff
Reuben Avery <ravery@bitswarm.io>
2016-05-30 14:26:13 -0400

    creating bsl_bootstrap::puppetmaster::done

---
+++ manifests/puppetmaster/done.pp
@@ -0,0 +1,31 @@
+# Class: bsl_bootstrap::puppetmaster::done
+# ===========================
+#
+# To be invoked after bootstrapping is successful, cleans up init scripts
+#
+# Authors
+# -------
+#
+# Reuben Avery <ravery@bitswarm.io>
+#
+# Copyright
+# ---------
+#
+# Copyright 2016 Bitswarm Labs
+#
+class bsl_bootstrap::puppetmaster::done {
+  include 'bsl_bootstrap'
+  include 'bsl_bootstrap::puppetmaster::config'
+
+  notify { 'bsl_bootstrap done':
+    message => '#### bsl_bootstrap::puppetmaster::done',
+  }
+
+  file { $bsl_bootstrap::init_service:
+    ensure  => absent,
+  }
+
+  file { $bsl_bootstrap::init_config:
+    ensure  => absent,
+  }
+}
