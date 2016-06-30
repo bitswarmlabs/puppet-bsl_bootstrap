8cbec1b74bf58331ca1ed7dd5baddfb0863b90c6
Reuben Avery <ravery@bitswarm.io>
2016-06-30 10:30:03 -0400

    brute force debugging hacking

--- manifests/puppetmaster/bootstrapping.pp
+++ manifests/puppetmaster/bootstrapping.pp
@@ -1,6 +1,10 @@
-class bsl_bootstrap::puppetmaster::bootstrapping {
-  if ! defined(File['/etc/puppetlabs/code/bsl_bootstrap']) {
-    file { '/etc/puppetlabs/code/bsl_bootstrap':
+class bsl_bootstrap::puppetmaster::bootstrapping(
+  $code_dir = $bsl_bootstrap::params::code_dir,
+) inherits bsl_bootstrap::params {
+  include 'bsl_bootstrap::puppetmaster::config'
+
+  if ! defined(File[$code_dir]) {
+    file { $code_dir:
       ensure => directory
     }
   }
--- manifests/puppetmaster/bootstrapping/hiera.pp
+++ manifests/puppetmaster/bootstrapping/hiera.pp
@@ -1,26 +1,22 @@
 class bsl_bootstrap::puppetmaster::bootstrapping::hiera {
+  include 'bsl_bootstrap::puppetmaster::bootstrapping'
   include 'bsl_bootstrap::puppetmaster::config'
 
-  $github_api_token = hiera('github_api_token', false)
-  $puppetmaster_fqdn = hiera('puppetmaster', 'puppet')
-  $default_admin_acct_name = hiera('default_admin_acct_name', 'admin')
-  $default_admin_acct_pass = hiera('default_admin_acct_pass', 'admin')
-  $server_external_fqdn = hiera('external_fqdn', $::fqdn)
-
-  file { '/etc/puppetlabs/code/bsl_bootstrap/hiera.yaml':
+  $code_dir = $bsl_bootstrap::puppetmaster::bootstrapping::code_dir
+  file { "${$code_dir}/hiera.yaml":
     ensure  => file,
     source  => 'puppet:///modules/bsl_bootstrap/puppetmaster/bootstrapping-hiera.yaml',
-    require => File['/etc/puppetlabs/code/bsl_bootstrap'],
+    require => File[$code_dir],
   }
 
-  file { '/etc/puppetlabs/code/bsl_bootstrap/hieradata':
+  file { "${$code_dir}/hieradata":
     ensure  => directory,
-    require => File['/etc/puppetlabs/code/bsl_bootstrap'],
+    require => File[$code_dir],
   }
 
-  file { '/etc/puppetlabs/code/bsl_bootstrap/hieradata/puppetmaster.yaml':
+  file { "${$code_dir}/hieradata/puppetmaster.yaml":
     ensure  => file,
     content => template('bsl_bootstrap/hieradata-puppetmaster-yaml.erb'),
-    require => File['/etc/puppetlabs/code/bsl_bootstrap'],
+    require => File[$code_dir],
   }
 }
